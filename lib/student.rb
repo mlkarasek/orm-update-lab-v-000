f6845226db2e4cdc5908a5aaff2c5012aeceaee1
Monica  Karasek <mlkarasek@gmail.com>
2018-08-05 02:12:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,11 +41,10 @@ attr_accessor :id, :name, :grade
   end
 
   def self.new_from_db(row)
-    student = Student.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
 end
 
 def self.find_by_name(name)
